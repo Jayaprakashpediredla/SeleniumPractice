0229c25835eabf1b80dbc6f14a2dc5a5233486de
Pedir <pediredlajayaprakash@gmail.com>
2024-04-06 10:47:09 +0530

    Login Test demo

---
+++ src/test/java/LoginTest.java
@@ -0,0 +1,20 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class LoginTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		//WebDriverManager.chromeDriver().setup();
+		WebDriver driver = new ChromeDriver();
+		driver.get("https://www.saucedemo.com/");
+		
+		driver.findElement(By.id("user-name")).sendKeys("standard_user");
+		driver.findElement(By.id("password")).sendKeys("secret_sauce");
+		driver.findElement(By.xpath("/html/body/div/div/div[2]/div[1]/div/div/form/input")).click();
+		driver.close();
+	}
+
+}
